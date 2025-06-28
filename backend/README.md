9e9e90f719119d785e5223d85655d9379b0dd4e2
Mohamed Tahiri <moha.tah@icloud.com>
2025-06-28 18:19:59 +0200

    fix: some changes to be more readable

--- backend/README.md
+++ backend/README.md
@@ -1,13 +1,35 @@
-Run backend
+# Backend
 
+For this project, we use Python 3.10+, Flask, and SQLite.
+
+## Installation
+
+Python 3.10+ is required.
+You can run the following script to install the dependencies.
 ```
-flask --app app run -p 5001
+./install.sh // OR ./install3.sh for MacOS where python3 is the default python
 ```
+
+## Running the backend
+
+Run the following script to start the backend.
+```
+./start.sh
+```
+
+## Creating a new game
+
+Run the following command at root to create a new game (word).
+```
+. backend/.venv/bin/activate && python -m backend.automation.daily_game_creation
+```
+
+
+## Cron job for game creation
 
 Automate game creation using crontable and specific script
 
 ```
 crontab -e
-
 45 9 * * 1-5 cd <path/to/wordle/file> && . backend/.venv/bin/activate && python -m backend.automation.daily_game_creation > wordle.log
 ```
--- frontend/app/actions.ts
+++ frontend/app/actions.ts
@@ -1,35 +1,44 @@
 'use server'
 
-import { redirect } from "next/navigation";
-import { getHeaders } from "./connection";
+import { redirect } from 'next/navigation'
+import { getHeaders } from './connection'
 
 export const redirectToAllGames = async () => {
-    redirect("/")
+  redirect('/')
 }
 
 export const fetchUserInfo = async () => {
-    //TODO: save into session to create cache (or just use next cache)
-    const result_raw = await fetch(`http://localhost:5001/user/current`, {
-        method: "GET",
-        headers: await getHeaders()
-    });
-    return await result_raw.json();
+  // TODO: save into session to create cache (or just use next cache)
+  const result_raw = await fetch(`http://localhost:5001/user/current`, {
+    method: 'GET',
+    headers: await getHeaders(),
+  })
+  return await result_raw.json()
 }
-
 
 export const fetchAllGames = async () => {
-    const result_raw = await fetch(`http://localhost:5001/game`, {
-        method: "GET",
-        headers: await getHeaders()
-    },);
-    const results_raw = await result_raw.json()
-    const results = results_raw.games.map((result: { game_id: number, word_length: number, start_date: string, state: string, locked: boolean }) => ({
-        id: result.game_id,
-        wordLength: result.word_length,
-        locked: result.locked,
-        date: result.start_date,
-        state: result.state,
-    }));
-    return results.sort((a: { date: string; }, b: { date: string; }) => new Date(b.date).getTime() - new Date(a.date).getTime())
+  const result_raw = await fetch(`http://localhost:5001/game`, {
+    method: 'GET',
+    headers: await getHeaders(),
+  })
+  const results_raw = await result_raw.json()
+  const results = results_raw.games.map(
+    (result: {
+      game_id: number
+      word_length: number
+      start_date: string
+      state: string
+      locked: boolean
+    }) => ({
+      id: result.game_id,
+      wordLength: result.word_length,
+      locked: result.locked,
+      date: result.start_date,
+      state: result.state,
+    })
+  )
+  return results.sort(
+    (a: { date: string }, b: { date: string }) =>
+      new Date(b.date).getTime() - new Date(a.date).getTime()
+  )
 }
-
